8f975b572d46f5201d39047b090b0cea45284da2
codec-onion <bonnetmatthieu86@gmail.com>
2026-06-18 21:50:06 +0200

    install express backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,16 @@
-import {User} from "@reparthune/shared"
+import express, { Request, Response, urlencoded } from "express"
 
-let test: User = {
-  _id: "cp",
-  email: "vfv",
-  createdAt: new Date()
-}
+
+const app = express()
+
+app.use(express.json())
+app.use(urlencoded({ extended: true }))
+app.get("/health", (req: Request, res: Response) => {
+  res.status(200).json("Coucou")
+})
+
+const PORT = process.env.PORT || 8081
+
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${PORT}`)
+})
